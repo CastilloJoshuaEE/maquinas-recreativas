1518dfac09d659a2f580f135bb8f21ddacea6b1c
CastilloJoshuaEE <castle2004josh2@gmail.com>
2026-06-29 14:51:42 -0500

    eliminando la carpeta de backend

--- backend/application/queries/maquina/ObtenerTodasMaquinasQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Application/Queries/Maquina/ObtenerTodasMaquinasQuery.php
-
-namespace maquinas_recreativas\Application\Queries\Maquina;
-
-final class ObtenerTodasMaquinasQuery
-{
-    // Sin parámetros por ahora
-}
--- backend/scripts/hash.php
+++
@@ -1,4 +0,0 @@
-<?php
-// backend/scripts/hash.php
-echo password_hash("12345678", PASSWORD_DEFAULT);
-?>
